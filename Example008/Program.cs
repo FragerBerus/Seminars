9f48bb3e8841de60167b71695a58bbf7550e3fca
Berus Frager <gitmailru@yandex.ru>
2022-08-08 11:53:27 +0300

    Коммит задачи №8

---
+++ Example008/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8: 
+// Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+int count = 1;
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Ряд четных чисел в диапазоне от 1 до " + number);
+while (count <= number)
+    {       
+        if (count % 2 == 0)
+        {
+            Console.WriteLine(count);
+        }
+        count ++;        
+    }
